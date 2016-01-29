7ed8d4ddf5172ba9e31b82eaf6968daef93ee166
Gaston Briones <mauricio_briones@artear.com.ar>
2016-01-29 19:09:26 -0300

    fix

--- wp-content/themes/bacare/footer.php
+++ wp-content/themes/bacare/footer.php
@@ -1,7 +1,7 @@
 <footer>
     Footer
     <?php wp_footer(); ?>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.bxslider.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-2.1.3.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.bxslider.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
 </footer>
--- wp-content/themes/bacare/js/init.js
+++ wp-content/themes/bacare/js/init.js
@@ -1,5 +1,13 @@
 
 (function ($) {
 
+    $('.product-list').bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 154,
+        slideMargin:15,
+        pager: false,
+        infiniteLoop: false
+    });
 
 })(jQuery);
