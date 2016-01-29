24f184dda96ce975e33bf825b3c08f9d5bf08144
Gaston Briones <gbriones@cmd.com.ar>
2016-01-28 22:55:33 -0300

    init commit

---
+++ wp-content/themes/bacare/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function register_my_menus()
+{
+    register_nav_menus(
+        array(
+            'header-menu' => __('Header Menu'),
+            'category-menu' => __('Category Menu'),
+            'sticky-menu' => __('Sticky Menu')
+        )
+    );
+}
+
+add_action('init', 'register_my_menus');
+
+
+add_filter( 'nav_menu_link_attributes', 'filter_function_name', 10, 3 );
+
+function filter_function_name( $atts, $item, $args ) {
+    // Manipulate attributes
+    return $atts;
+}
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
---
+++ wp-content/themes/bacare/index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<body <?php body_class(); ?>>
+<div id="site">
+    <?php get_template_part('content-header'); ?>
+    <?php get_template_part('content-main');?>
+    <?php get_footer();?>
+</div>
+</body>
+</html>
